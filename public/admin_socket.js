d3487579a0ef3260b8ebcde4afee3619ed3aa23d
zainyazdan <zainyazdan54321@gmail.com>
2020-12-03 05:09:57 +0500

    Live status added

---
+++ public/admin_socket.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('userJoined', (data) => {
+
+    console.log("userJoined() : ", data);
+
+    document.getElementById('socket-data').innerHTML += 'Joined : '  + data.time + '<br>'
+})
+
+
+socket.on('userLeaved', (data) => {
+
+    console.log("userLeaved() : ", data);
+    document.getElementById('socket-data').innerHTML += 'Leaved : '  + data.time + '<br>'
+
+})
+
+
--- public/socket.js
+++ public/socket.js
@@ -2,19 +2,19 @@ var socket = io();
 
 socket.emit('joinedSite', 'I am here');
 
-socket.on('userJoined', (data) => {
+// socket.on('userJoined', (data) => {
 
-    console.log("userJoined() : ", data);
+//     console.log("userJoined() : ", data);
 
-    document.getElementById('socket-data').innerHTML += 'Joined : '  + data.time + '<br>'
-})
+//     document.getElementById('socket-data').innerHTML += 'Joined : '  + data.time + '<br>'
+// })
 
 
-socket.on('userLeaved', (data) => {
+// socket.on('userLeaved', (data) => {
 
-    console.log("userLeaved() : ", data);
-    document.getElementById('socket-data').innerHTML += 'Leaved : '  + data.time + '<br>'
+//     console.log("userLeaved() : ", data);
+//     document.getElementById('socket-data').innerHTML += 'Leaved : '  + data.time + '<br>'
 
-})
+// })
 
 
